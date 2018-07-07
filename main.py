a391eed2744fdcc395e0f80634b86d4fb88d9380
Mariya Snow <mariyavsnow@gmail.com>
2018-07-07 14:12:38 -0700

    main() code sample

---
+++ main.py
@@ -0,0 +1,14 @@
+#################
+# @file - main.py
+# @author - Mariya Snow <mariyasnow@gmail.com>
+# @brief - Demo main()
+# @usage
+'''bash-3.2$ python FlyingCircus.py
+Nobody expects the Spanish Inquisition!
+'''
+#################
+def main():
+   print("Nobody expects the Spanish Inquisition!")
+
+if __name__ == '__main__':
+   main()
